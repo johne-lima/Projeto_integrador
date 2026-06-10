5e0da605f3f03e935066a62d6d50aa0a2b416f3f
Johne Lima <jk.lima014@gmail.com>
2026-06-10 10:12:10 -0300

    atualizado erro de slide

--- scripts/script.js
+++ scripts/script.js
@@ -12,50 +12,6 @@ menuBtn.addEventListener('click', () => {
 
 })
 
-const splide = new Splide('.splide', {
-    rewind: true,
-    autoplay: true,
-    interval: 2000,
-
-    perPage: 2,
-    focus: 'center',
-    gap: '1rem',
-    padding: '15%',
-
-    drag: true,
-    snap: true,
-    updateOnMove: true,
-    waitForTransition: true,
-
-    breakpoints: {
-        768: {
-            perPage: 2,
-            padding: '10%',
-        },
-
-        500: {
-            perPage: 1,
-            padding: '5%',
-        }
-    }
-});
-
-splide.mount();
-
-document.querySelectorAll('.splide__slide').forEach((slide, index) => {
-    slide.style.cursor = 'pointer';
-
-    slide.addEventListener('click', () => {
-        splide.go(index);
-    });
-});
-
-document.querySelectorAll('.splide__slide').forEach(slide => {
-    slide.addEventListener('click', () => {
-        splide.go(Number(slide.dataset.slide));
-    });
-});
-
 const form = document.querySelector('.formulario');
 
 if (form) {
